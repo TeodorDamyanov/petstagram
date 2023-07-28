37e75a439879454c9e32a5d62aa285f05e6a59ef
Teodor <114014071+TeodorDamyanov@users.noreply.github.com>
2023-07-28 23:10:58 +0300

    Add files via upload

---
+++ petstagram/pets/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.template.defaultfilters import slugify
+
+
+class Pet(models.Model):
+    name = models.CharField(max_length=30)
+    personal_pet_photo = models.URLField()
+    date_of_birth = models.DateField(blank=True, null=True)
+    slug = models.SlugField(unique=True, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if not self.slug:
+            self.slug = slugify(f"{self.name}-{self.pk}")
+        return super().save(*args, **kwargs)
